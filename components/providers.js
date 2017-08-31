40fc6fdcff7f010b339076b605f83fef0ab77df3
Rafael Conde <rafahari@me.com>
2017-08-31 18:37:06 +0100

    Add providers icons and more styling

--- components/providers.js
+++ components/providers.js
@@ -7,11 +7,14 @@ class Providers extends Nanocomponent {
   createElement () {
     return html`
       <div class="${styles.providersGroup}">
-        <div class="${styles.providersSeparator}">or</div>
-        <button class="${styles.providerGoogle} ${styles.btn}">Google</button>
-        <button class="${styles.providerGitHub} ${styles.btn}">GitHub</button>
-        <button class="${styles.providerGitLab} ${styles.btn}">Gitlab</button>
-        <button class="${styles.providerBitbucket} ${styles.btn}">Bitbucket</button>
+        <div class="${styles.providersSeparator}">
+          <span class="${styles.separatorLine}"></span>
+          <span>or</span>
+        </div>
+        <button class="${styles.providerGoogle} ${styles.btn}"><span>Login with Google</span></button>
+        <button class="${styles.providerGitHub} ${styles.btn}"><span>Login with GitHub</span></button>
+        <button class="${styles.providerGitLab} ${styles.btn}"><span>Login with Gitlab</span></button>
+        <button class="${styles.providerBitbucket} ${styles.btn}"><span>Login with Bitbucket</span></button>
       </div>
     `;
   }
