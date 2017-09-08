f658218afe2f1c88e311d25b4b4970a8f561e1f2
Bret Comnes <bcomnes@gmail.com>
2017-09-08 14:59:49 -0700

    Integrate UMD and module release

--- script/release.js
+++ script/release.js
@@ -1,17 +1,15 @@
-var exec = require('child_process').execSync
-var version = require("../package.json").releaseVersion;
+/* eslint no-console: "off" */
+const exec = require("child_process").execSync;
+const semver = require("semver");
+const version = "v" + semver(require("../package.json").version).major;
 
-console.log('Building new release for ', version);
+console.log("Building new release for ", version);
 
-exec("yarn")
-exec("yarn build")
+exec("yarn");
+exec("yarn build");
 
-console.log('Creating release dir')
-exec("mkdir -p releases/" + version)
+console.log("Creating release dir");
+exec("mkdir -p releases/" + version);
 
-console.log('copying release files');
-exec("cp build/* releases/" + version)
-
-console.log('git commit')
-exec("git add releases/" + version)
-exec("git commit -m 'Created new release for version " + version + "'")
+console.log("copying release files");
+exec("cp build/* releases/" + version);
