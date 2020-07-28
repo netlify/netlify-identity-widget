54ed057e06e1a97e5573f97f671eae62d1e94fa4
Esteban Rojas <estebanjrojas@gmail.com>
2020-07-27 23:41:31 -0600

    feat: adding Spanish translation
    Agregando la traducción al español

--- src/translations/index.js
+++ src/translations/index.js
@@ -1,8 +1,9 @@
 import * as en from "./en.json";
 import * as fr from "./fr.json";
+import * as es from "./es.json";
 
 export const defaultLocale = "en";
-const translations = { en, fr };
+const translations = { en, fr, es };
 
 export const getTranslation = (key, locale = defaultLocale) => {
   const translated = translations[locale] && translations[locale][key];
