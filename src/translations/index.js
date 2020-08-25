1fbb00ca7a958dea1e93e68f3c285374c08b551e
Daniel Soares <daniel.soares@me.com>
2020-08-25 15:23:48 -0300

    feat: Portuguese translation (#352)
    Co-authored-by: amarilis <6574688+amarilisd@users.noreply.github.com>

--- src/translations/index.js
+++ src/translations/index.js
@@ -2,9 +2,10 @@ import * as en from "./en.json";
 import * as fr from "./fr.json";
 import * as es from "./es.json";
 import * as hu from "./hu.json";
+import * as pt from "./pt.json";
 
 export const defaultLocale = "en";
-const translations = { en, fr, es, hu };
+const translations = { en, fr, es, hu, pt };
 
 export const getTranslation = (key, locale = defaultLocale) => {
   const translated = translations[locale] && translations[locale][key];
