74feb1499f9f813adee31766c0d3c62f8d1ecbf8
Yuming Cheung <ymcheung.tw@gmail.com>
2020-09-02 20:44:20 +0800

    Update locale & test

--- src/translations/index.js
+++ src/translations/index.js
@@ -3,9 +3,10 @@ import * as fr from "./fr.json";
 import * as es from "./es.json";
 import * as hu from "./hu.json";
 import * as pt from "./pt.json";
+import * as zhTW from "./zhTW.json";
 
 export const defaultLocale = "en";
-const translations = { en, fr, es, hu, pt };
+const translations = { en, fr, es, hu, pt, zhTW };
 
 export const getTranslation = (key, locale = defaultLocale) => {
   const translated = translations[locale] && translations[locale][key];
