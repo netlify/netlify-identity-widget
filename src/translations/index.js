38d6d1e066d859856a4436d7e318c68aa8ffa1f7
Marie Rigal <marierigal4@gmail.com>
2020-10-14 19:55:58 +0200

    feat(trans): add ru locale to app and tests

--- src/translations/index.js
+++ src/translations/index.js
@@ -3,9 +3,10 @@ import * as fr from "./fr.json";
 import * as es from "./es.json";
 import * as hu from "./hu.json";
 import * as pt from "./pt.json";
+import * as ru from "./ru.json";
 
 export const defaultLocale = "en";
-const translations = { en, fr, es, hu, pt };
+const translations = { en, fr, es, hu, pt, ru };
 
 export const getTranslation = (key, locale = defaultLocale) => {
   const translated = translations[locale] && translations[locale][key];
