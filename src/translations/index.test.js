b40554aea224fbf6b20652bb40f6de2bd908f955
Cassidy Williams <1454517+cassidoo@users.noreply.github.com>
2021-05-27 18:00:33 -0500

    Remove trailing spaces (again)

--- src/translations/index.test.js
+++ src/translations/index.test.js
@@ -32,7 +32,7 @@ describe("translations", () => {
     const { getTranslation } = require("./");
     expect(getTranslation("log_in", "pt")).toEqual("Entrar");
   });
-  
+
   it("should return translation for 'pl' locale", () => {
     const { getTranslation } = require("./");
     expect(getTranslation("log_in", "pl")).toEqual("Zaloguj się");
