1a605dceacfc74ec8784f846af870676af6529c7
alexia980 <138984110+alexia980@users.noreply.github.com>
2023-07-08 13:01:44 +0000

    added greek translation

--- src/translations/index.js
+++ src/translations/index.js
@@ -1,5 +1,6 @@
 import * as en from "./en.json";
 import * as fr from "./fr.json";
+import * as es from "./el.json";
 import * as es from "./es.json";
 import * as hu from "./hu.json";
 import * as pt from "./pt.json";
@@ -8,7 +9,7 @@ import * as cs from "./cs.json";
 import * as sk from "./sk.json";
 
 export const defaultLocale = "en";
-const translations = { en, fr, es, hu, pt, pl, cs, sk };
+const translations = { en, fr, el, es, hu, pt, pl, cs, sk };
 
 export const getTranslation = (key, locale = defaultLocale) => {
   const translated = translations[locale] && translations[locale][key];
