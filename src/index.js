c3da521060db5b50bfa3e82a2bc3761ba4224d3a
Karin Hendrikse <30577427+khendrikse@users.noreply.github.com>
2026-01-30 17:05:55 +0100

    fix: correct typeof comparisons and modernize ESLint to v9 (#676)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import netlifyIdentity from "./netlify-identity";
 
-if (typeof exports !== undefined) {
+if (typeof exports !== "undefined") {
   exports.netlifyIdentity = netlifyIdentity;
 }
-if (typeof window !== undefined) {
+if (typeof window !== "undefined") {
   window.netlifyIdentity = netlifyIdentity;
 }
 if (document.readyState === "loading") {
